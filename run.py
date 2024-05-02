ddc86c197f1f3cca9a53048ad5710412be8f95ce
Shoua Thao <s.thao008@gmail.com>
2024-05-02 14:12:52 +0200

    fixed "template not found" error AB#13

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.main.routes import main
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates')
 app.config.from_object('config.Config')
 
 app.register_blueprint(main)
