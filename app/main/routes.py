e001f6058c0d238efbc33c6fece42b9afa97d314
Shoua Thao <s.thao008@gmail.com>
2024-05-22 20:59:40 +0200

    inplmented mobin's model and result page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 from flask import render_template, Blueprint
+import torch
+from torchvision import models
+import os
+import base64
+import uuid
 
 from .forms import PhotoForm
+from .utils import transform_image, get_prediction, save_image
 
 main = Blueprint('main', __name__)
 
@@ -10,7 +16,16 @@ def index():
     form = PhotoForm()
 
     if form.validate_on_submit():
-        f = form.photo.data
-        return "Photo submitted successfully!"
-
+        file = form.photo.data
+        img_bytes = file.read()
+        prediction, confidence = get_prediction(img_bytes)
+        encoded_img_bytes = base64.b64encode(img_bytes).decode('utf-8')
+        
+        # We should do this sometime, but with proper handling of making folder if we don't have one for feedback_data
+        unique_filename = str(uuid.uuid4()) + ".jpg"
+        # class_folder = os.path.join('C:/python/maskin inlärning/first_cnn_with_chatgpt_4o/feedback_data', prediction)
+        # save_image(img_bytes, class_folder, unique_filename)
+        
+        return render_template('result.html', prediction=prediction, confidence=confidence, image_bytes=encoded_img_bytes, filename=unique_filename)
+    
     return render_template('index.html', form=form)
